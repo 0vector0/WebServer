b751d6919aa10d331fcba8ea7aec21a77c6538fc
0vector0 <0vector0@gmail.com>
2016-06-04 23:39:35 +0300

    try to write multy client chat

---
+++ src/com/github/mykhalechko/chat/ServerOperation.java
@@ -0,0 +1,19 @@
+package com.github.mykhalechko.chat;
+
+import java.net.Socket;
+
+/**
+ * Created by vector on 04.06.2016.
+ */
+public class ServerOperation implements Runnable{
+
+    ServerOperation(Socket client){
+
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
